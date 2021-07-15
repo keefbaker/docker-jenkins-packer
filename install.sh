dc46cbf3bf89c30425232e4ac92d9537d33742aa
Keith Baker <keith.baker@arm.com>
2021-07-15 07:45:28 +0100

    add script
    Change-Id: I783c6a5187aee66992397f66df78428101dc9f33

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update && apt-get install -y python3 python3-pip unzip
+python3 -m pip install boto3 
+apt-get clean
+curl https://releases.hashicorp.com/packer/1.6.5/packer_1.6.5_linux_amd64.zip -o packer_1.6.5_linux_amd64.zip
+unzip -o packer_1.6.5_linux_amd64.zip
+chmod 755 packer
+rm -rf packer_1.6.5_linux_amd64.zip
+curl https://omnitruck.chef.io/install.sh | bash -s -- -P inspec
