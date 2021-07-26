609170ab8b305609e6a0c96324f7ee47b731e860
Keith Baker <keith.baker@arm.com>
2021-07-26 07:37:01 +0100

    also purge pip
    Change-Id: I654de76f07bf4478fc53eb527f234edc987e42b3

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update && apt-get install -y python3 python3-pip unzip
 python3 -m pip install boto3
+python3 -m pip cache purge
 apt-get clean
 curl https://releases.hashicorp.com/packer/1.7.3/packer_1.7.3_linux_amd64.zip -o packer_1.7.3_linux_amd64.zip
 unzip -o packer_1.7.3_linux_amd64.zip
